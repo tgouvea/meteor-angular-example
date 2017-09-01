841ab17ce5be5658dae6d056bb3de72e36dfc982
Tiago Gouvêa <tr617080@oi.corp.net>
2017-09-01 18:54:54 -0300

    Filtering collections

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -3,60 +3,77 @@ import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
 import { Tasks } from '../../api/tasks.js';
 
- 
+
 class TodosListCtrl {
-  constructor($scope) {
+    constructor($scope) {
 
-    $scope.viewModel(this);
+        $scope.viewModel(this);
 
-    this.helpers({
+        this.hideCompleted = false;
 
-        tasks(){
+        this.helpers({
 
-        // Show newest tasks at the top
-        return Tasks.find({}, {
-            sort: {
-              createdAt: -1
+            tasks() {
+
+                const selector = {};
+
+                // If hide completed is checked, filter tasks
+                if (this.getReactively('hideCompleted')) {
+                    selector.checked = {
+                        $ne: true
+                    };
+                }
+
+                // Show newest tasks at the top
+                return Tasks.find(selector, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+
+            },
+            incompleteCount() {
+              return Tasks.find({
+                checked: {
+                  $ne: true
+                }
+              }).count();
             }
-          });
-
-        }
-
-    })
-  }
+        })
+    }
 
 
-  addTask(newTask) {
-    // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date
-    });
- 
-    // Clear form
-    this.newTask = '';
-  }
+    addTask(newTask) {
+        // Insert a task into the collection
+        Tasks.insert({
+            text: newTask,
+            createdAt: new Date
+        });
 
-   
-  setChecked(task) {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      },
-    });
-  }
- 
-  removeTask(task) {
-    Tasks.remove(task._id);
-  }
+        // Clear form
+        this.newTask = '';
+    }
+
+
+    setChecked(task) {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(task._id, {
+            $set: {
+                checked: !task.checked
+            },
+        });
+    }
+
+    removeTask(task) {
+        Tasks.remove(task._id);
+    }
 
 }
- 
+
 export default angular.module('todosList', [
-  angularMeteor
+    angularMeteor
 ])
-  .component('todosList', {
-    templateUrl: 'imports/components/todosList/todosList.html',
-    controller: ['$scope', TodosListCtrl]
-  });
+    .component('todosList', {
+        templateUrl: 'imports/components/todosList/todosList.html',
+        controller: ['$scope', TodosListCtrl]
+    });
