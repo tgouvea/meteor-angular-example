bcf7454fadacd98fd179f86b17b7cf3151dae81b
Tiago Gouvêa <tr617080@oi.corp.net>
2017-09-01 17:30:49 -0300

    Adding tasks with a form

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -13,12 +13,30 @@ class TodosListCtrl {
 
         tasks(){
 
-            return Tasks.find({});
+        // Show newest tasks at the top
+        return Tasks.find({}, {
+            sort: {
+              createdAt: -1
+            }
+          });
 
         }
 
     })
   }
+
+
+  addTask(newTask) {
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+ 
+    // Clear form
+    this.newTask = '';
+  }
+
 }
  
 export default angular.module('todosList', [
